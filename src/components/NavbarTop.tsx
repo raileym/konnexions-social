c0c50ff6610d814dde19781baf344ccef7e8a168
raileym <public@raileym.net>
2025-05-14 12:04:02 -0400

    Looks okay. Let's move on to add function points.

--- src/components/NavbarTop.tsx
+++ src/components/NavbarTop.tsx
@@ -7,15 +7,16 @@ import Button from './Button'
 const NavbarTop: React.FC = () => {
   return (
     <nav className="fixed top-0 shadow-3 left-0 w-100 bg-whiteX dark-grayX bg-white flex items-center justify-between ph3 pv2 z-999">
-      <div className="flex items-center">
-        <div className="bg-brand mr4">
+      <div className="flex items-centerX justify-start baX w-80X">
+        <div className="bg-brand mr3 ba b--white bw2 h3X ml2X">
           <img
             src="/logo-blue-transparent-on-white-173x126.png"
             alt="CKN logo"
-            className="db aspect-ratio aspect-ratio--1x h3"
+            className="db aspect-ratio aspect-ratio--1x h3X mv0X"
+            style={{height: '3em'}}
           />
         </div>
-        <div className="black-30 ml2 f3 b lh-title tl">
+        <div className="black-30 ml2X f3 b baX lh-title tlX">
           <div><span className="brand">CK</span>Ո Series on Joy</div>
           <div className="black-20 f5">Joy of Language - Spanish</div>
         </div>
