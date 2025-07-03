6a62a790490c3b6ff95581088cc9660307143627
raileym <public@raileym.net>
2025-07-03 10:38:46 -0400

    Looks excellent.

--- src/components/PanelGenAIPro/PanelGenAIProComponents/formatFlexLesson/formatFlexLesson.ts
+++ src/components/PanelGenAIPro/PanelGenAIProComponents/formatFlexLesson/formatFlexLesson.ts
@@ -3,11 +3,13 @@ import type { FlexLesson, FormattedFlexLesson } from '@cknTypes/types'
 type FormatFlexLessonProps = {
   flexLesson: FlexLesson
 }
+
 export const formatFlexLesson = ({ flexLesson }: FormatFlexLessonProps): FormattedFlexLesson => {
   const formattedFlexLesson = flexLesson
-    .split(/\r\n|\n|\r/)
-    .map(line => `n|narrator|${line.trim()}`)
-    .filter(line => line.length > 0)
+    .split(/\r\n|\n|\r/)               // split into lines
+    .map(line => line.trim())          // trim whitespace
+    .filter(line => line.length > 0)  // remove empty lines
+    .map(line => `n|narrator|${line}`) // add prefix after filtering
 
   return formattedFlexLesson
 }
