6764c0c6473472affe2ab1b955a809c45f1cf0eb
raileym <public@raileym.net>
2025-07-13 11:15:21 -0400

    Removed backslashes.

--- src/components/formatFlexLesson/formatFlexLesson.ts
+++ src/components/formatFlexLesson/formatFlexLesson.ts
@@ -6,10 +6,11 @@ type FormatFlexLessonProps = {
 
 export const formatFlexLesson = ({ flexLesson }: FormatFlexLessonProps): FormattedFlexLesson => {
   const formattedFlexLesson = flexLesson
-    .split(/\r\n|\n|\r/)               // split into lines
-    .map(line => line.trim())          // trim whitespace
-    .filter(line => line.length > 0)  // remove empty lines
-    .map(line => `n|narrator|${line}`) // add prefix after filtering
+    .split(/\r\n|\n|\r/)                    // split into lines
+    .map(line => line.replace(/\\/g, ''))   // remove all backslashes
+    .map(line => line.trim())              // trim whitespace
+    .filter(line => line.length > 0)       // remove empty lines
+    .map(line => `n|narrator|${line}`)     // add prefix after filtering
 
   return formattedFlexLesson
 }
