fb4a6d753092e282609d1c1bdb9accb4266e1b48
raileym <public@raileym.net>
2025-07-19 21:29:17 -0400

    Good enough. Keep working it.

--- src/components/MyKonnexionsSlash/MyKonnexionsSlash.tsx
+++ src/components/MyKonnexionsSlash/MyKonnexionsSlash.tsx
@@ -19,13 +19,15 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
   }
 
   let slashTop: string,
-    slashLeft: string,
-    // mhX: number,
-    // mlX: number,
-    // mrX: number,
-    mMarginLeft: string,
-    mMarginRight: string,
-    marginTop: string
+      slashWidth: string,
+      slashLeft: string,
+      // mhX: number,
+      // mlX: number,
+      // mrX: number,
+      mWidth: string,
+      mMarginLeft: string,
+      mMarginRight: string,
+      marginTop: string
 
   if (lineHeight == 2) {
     slashTop = `${0.2 + 0.8}rem`
@@ -58,9 +60,48 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
 
   slashTop = '0.2rem'
   slashLeft = '0.1rem'
+  slashWidth = '0.5rem'
   mMarginLeft = '0.3rem' 
-  mMarginRight = '0.6rem' 
-  marginTop = '0.5rem' // `${lineHeight * -0.5}rem`
+  mMarginRight = '0.6rem'
+  mWidth = '0.3rem'
+  marginTop = '0.5rem'
+
+  switch (lineHeight) {
+    case 3: marginTop = '0.5rem'
+            mWidth = '0.5rem'
+            mMarginLeft = '0.5rem' 
+            mMarginRight = '0.6rem'
+            slashTop = '0.0rem'
+            slashWidth = '0.3rem'
+            slashLeft = '0.3rem'
+            break;
+    case 2: marginTop = '0.5rem'
+            mWidth = '0.3rem'
+            mMarginLeft = '0.3rem' 
+            mMarginRight = '0.6rem'
+            slashTop = '0.2rem'
+            slashWidth = '0.3rem'
+            slashLeft = '0.1rem'
+            break;
+    case 1.5: marginTop = '0.9rem'
+            mWidth = '0.3rem'
+            mMarginLeft = '0.3rem' 
+            mMarginRight = '0.4rem'
+            slashTop = '0.7rem'
+            slashWidth = '0.2rem'
+            slashLeft = '0.1rem'
+            break;
+    case 1: marginTop = '0.5rem'
+            mWidth = '0.3rem'
+            mMarginLeft = '0.3rem' 
+            mMarginRight = '0.6rem'
+            slashWidth = '0.5rem'
+            slashTop = '0.2rem'
+            slashLeft = '0.1rem'
+            break;
+  }
+
+  // marginTop = '0.5rem' // `${0.8*lineHeight * 0.5}rem` // '0.5rem' // `${lineHeight * -0.5}rem`
 
     if ( asSpan ) {
     return (
@@ -72,8 +113,8 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
                 marginTop: `${marginTop}`,
                 marginLeft: `${mMarginLeft}`,
                 marginRight: `${mMarginRight}`,
-                width: '.22rem',
-                height: '.75rem', // '0.52rem',
+                width: mWidth,
+                height: `${ lineHeight * 0.5}rem`, // '1.2rem', //'0.52rem',
                 lineHeight: `${lineHeight}`,
                 backgroundColor: color // 'red'
                 }}
@@ -85,6 +126,7 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
                 fontSize: '0.6rem',
                 top: slashTop, // '1.2rem', // '0.2rem', // 
                 left: slashLeft,
+                '--slash-width': slashWidth,
                 '--slash-top': '-.5rem',
                 '--slash-height': '2.25rem', // '2.25rem',
                 '--slash-color': updatedSlashColor // `var(--${slashColor})`
@@ -106,7 +148,7 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
                 marginTop: `${marginTop}`,
                 marginLeft: `${mMarginLeft}`,
                 marginRight: `${mMarginRight}`,
-                width: '.5rem',
+                width: mWidth,
                 height: `${ lineHeight * 0.5}rem`, // '1.2rem', //'0.52rem',
                 lineHeight: `${lineHeight}`
                 }}
@@ -118,6 +160,7 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
                 fontSize: '0.6rem',
                 top: slashTop, // '1.2rem', // '0.2rem', // 
                 left: slashLeft,
+                '--slash-width': slashWidth,
                 '--slash-top': '-.4rem',
                 '--slash-height': '3.5rem', // '3.5rem',
                 '--slash-color': updatedSlashColor // `var(--${slashColor})`
@@ -125,7 +168,6 @@ const KonnexionsSlash = ({lineHeight = 1, slashColor = 'primary', color = 'silve
             >
               m
             </div>
-            {lineHeight}
           </div>
         </div>
       </>
