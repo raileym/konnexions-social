07a618ffdb3ca7d30ac4b3e37dcfee33c9884468
raileym <public@raileym.net>
2025-07-27 15:24:28 -0400

    Focus architecture.

--- src/components/DayNightToggle/DayNightToggle.tsx
+++ src/components/DayNightToggle/DayNightToggle.tsx
@@ -17,11 +17,11 @@ const DayNightToggle = () => {
     <div
       tabIndex={-1}
       aria-disabled={false}
-      className="day-night-toggle fixed top-0 right-0 z-9999 pa1 bg-white ba bw1 b--backgroundX br3 flex items-center bg-on-background background b--background"
-      style={{ right: '2rem', top: '7rem', fontFamily: 'monospace' }}
+      className="day-night-toggle fixed top-0 right-0 z-9999 pa1 bg-whiteX ba bw1 b--backgroundX br3 flex items-center bg-on-background background b--on-background"
+      style={{ right: '12rem', top: '8rem', fontFamily: 'monospace' }}
     >
-<button onClick={toggleMode} className={`f3 ph2 pv1 b--blackX b--solid bw1 bg-light-grayX focus:b--red focus:bg-tertiaryX focus:whiteX bw2X ${!isDay ? 'bg-black b-black' : 'bg-whiteX b-black'}`}>
-        {!isDay ? <FontAwesomeIcon className="yellowX ma1 bg-blackX" icon={faSun} /> : <FontAwesomeIcon className="ma1 bg-greenX blackX" icon={faMoon} />}
+      <button onClick={toggleMode} className={`f3 ph2 pv1 b--blackX b--solid bw1 bg-light-grayX focus:b--red focus:bg-tertiaryX focus:whiteX bw2X ${!isDay ? 'bg-white b-black' : 'bg-on-background b-green'}`}>
+        {!isDay ? <FontAwesomeIcon className="yellow ma1 bg-blackX" style={{width: '1.0rem'}} icon={faSun} /> : <FontAwesomeIcon className="ma1 bg-greenX background" style={{width: '1.0rem'}} icon={faMoon} />}
       </button>
     </div>
   )
